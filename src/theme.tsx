49b1640351558cefd56ebeed0d92a2db43a231b3
Vivek Sasidharan <vs@payspoon.com>
2020-01-09 10:03:45 +0530

    remove auto switching dark mode

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,6 +1,6 @@
 import {createMuiTheme} from '@material-ui/core/styles';
 import {red} from '@material-ui/core/colors';
-import {useMediaQuery, ThemeProvider} from '@material-ui/core';
+import {ThemeProvider} from '@material-ui/core';
 import React from 'react';
 
 /**
@@ -9,7 +9,6 @@ import React from 'react';
  * @return {JSX.Element} Material Theme.
  */
 function MaterialTheme(_props: any) {
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
 
   const theme = React.useMemo(
       () =>
@@ -24,10 +23,10 @@ function MaterialTheme(_props: any) {
             error: {
               main: red.A400,
             },
-            type: prefersDarkMode ? 'dark' : 'light',
+            type: 'dark',
           },
         }),
-      [prefersDarkMode],
+      [],
   );
 
   return (
