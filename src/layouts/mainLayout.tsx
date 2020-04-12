4251c4a3373f6b958ff93655c4112da15ee68776
Vivek Sasidharan <vivekthottathil@gmail.com>
2020-04-12 19:41:54 +0530

    updates

--- src/layouts/mainLayout.tsx
+++ src/layouts/mainLayout.tsx
@@ -5,28 +5,28 @@ import {makeStyles} from '@material-ui/core/styles';
 
 const useStyle = makeStyles({
   root: {
-    'display': 'flex',
-    'flex-direction': 'column',
-    'min-height': '100%',
-    'height': '100%',
-    'width': '100%',
-    'padding-top': '20px',
-    'padding-bottomn': '20px',
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%',
+    width: '100%',
+    paddingTop: '20px',
+    paddingBottomn: '20px',
   },
+  content: {
+    flex: '1',
+    display: 'flex',
+    flexDirection: 'column'
+  }
 });
 
 const MainLayout = (props: any) => {
   const styles = useStyle({});
   return (
     <Container maxWidth="md" classes={{root: styles.root}}>
-      <div className="content">{props.children}</div>
+      <div className={styles.content}>{props.children}</div>
       <Footer/>
       <style jsx>{`
-      .content {
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-      }
+
     `}</style>
     </Container>
   );
