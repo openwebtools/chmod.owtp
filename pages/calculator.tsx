c8a321666a366c0237f430665ea07877bb3dab59
Vivek Sasidharan <vivekthottathil@gmail.com>
2020-04-12 18:58:41 +0530

    code smell fix

--- pages/calculator.tsx
+++ pages/calculator.tsx
@@ -118,7 +118,7 @@ const Calculator = () => {
     var newPerms = { ...perms };
 
     if(event.target.checked) {
-
+      // code to be added.
     } else {
       newPerms.fileOptions.folderOptions.preserveRoot = event.target.checked;
     }
