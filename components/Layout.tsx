d2ac2ff846a2bb770124be9f9be9fe07672d53ef
Vivek Sasidharan <mail@viveks.dev>
2021-04-04 22:53:39 +0530

    footer update

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from "react";
 import Head from "next/head";
 import Footer from "./Footer";
 import Toolbar from "./Toolbar";
-import { Box, Portal } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 
 type Props = {
   children?: ReactNode;
@@ -11,18 +11,16 @@ type Props = {
 
 const Layout = ({ children, title }: Props) => {
   return (
-    <div>
+    <Flex direction="column" h="100%">
       <Head>
         <title>{title}</title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Toolbar />
-      <Box p={4}>{children}</Box>
-      <Portal>
-        <Footer />
-      </Portal>
-    </div>
+      <Box p={4} flex="1">
+        {children}
+      </Box>
+      <Footer />
+    </Flex>
   );
 };
 
