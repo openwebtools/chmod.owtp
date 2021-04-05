16accad6db9ced9680bc01f002ee0fd4cb53a0f7
Vivek Sasidharan <mail@viveks.dev>
2021-04-06 00:18:26 +0530

    toolbar set

--- components/Toolbar.tsx
+++ components/Toolbar.tsx
@@ -1,53 +1,99 @@
-import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { CloseIcon, HamburgerIcon, MoonIcon, SunIcon } from "@chakra-ui/icons";
 import {
+  Box,
   Button,
   Flex,
   Heading,
-  HStack,
   IconButton,
   Link,
   Spacer,
+  Stack,
   useColorMode,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 import NextLink from "next/link";
 
 const Toolbar = (props: any) => {
   const { colorMode, toggleColorMode } = useColorMode();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+
+  const handleColorModeClick = () => {
+    toggleColorMode();
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <Flex as="nav" align="center" mb={4} p={4} {...props}>
-      <NextLink href="/" passHref>
-        <Link>
-          <Heading as="h1" size="md">
-            chmod calculator
-          </Heading>
-        </Link>
-      </NextLink>
-      <Spacer />
-      <HStack spacing="24px">
+    <Flex
+      as="nav"
+      align="center"
+      p={4}
+      justify="space-between"
+      wrap="wrap"
+      {...props}
+      direction={["column", "row", "row", "row"]}
+    >
+      <Flex align="center" justify="space-between" w="100%">
         <NextLink href="/" passHref>
           <Link>
-            <Button aria-label="chmod Calculator">Calculator</Button>
-          </Link>
-        </NextLink>
-        <NextLink href="/syntax" passHref>
-          <Link>
-            <Button aria-label="chmod Syntax">Syntax</Button>
-          </Link>
-        </NextLink>
-        <NextLink href="/examples" passHref>
-          <Link>
-            <Button aria-label="chmod Examples">Examples</Button>
+            <Heading as="h1" size="md">
+              chmod calculator
+            </Heading>
           </Link>
         </NextLink>
         <IconButton
-          onClick={toggleColorMode}
-          aria-label={
-            colorMode === "dark" ? "Toggle Light Mode" : "Toggle Dark Mode"
-          }
-          icon={colorMode === "dark" ? <SunIcon /> : <MoonIcon />}
+          onClick={toggleMenu}
+          aria-label="Open Menu"
+          display={{ base: "block", md: "none" }}
+          icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+          variant="ghost"
         />
-      </HStack>
+      </Flex>
+
+      <Spacer />
+      <Box
+        display={{ base: isOpen ? "block" : "none", md: "block" }}
+        flexBasis={{ base: "100%", md: "auto" }}
+      >
+        <Stack
+          spacing={8}
+          align="center"
+          justify={["center", "space-between", "flex-end", "flex-end"]}
+          direction={["column", "row", "row", "row"]}
+          pt={[4, 4, 0, 0]}
+        >
+          <NextLink href="/" passHref>
+            <Link>
+              <Button aria-label="chmod Calculator" variant="ghost">
+                Calculator
+              </Button>
+            </Link>
+          </NextLink>
+          <NextLink href="/syntax" passHref>
+            <Link>
+              <Button aria-label="chmod Syntax" variant="ghost">
+                Syntax
+              </Button>
+            </Link>
+          </NextLink>
+          <NextLink href="/examples" passHref>
+            <Link>
+              <Button aria-label="chmod Examples" variant="ghost">
+                Examples
+              </Button>
+            </Link>
+          </NextLink>
+          <IconButton
+            onClick={handleColorModeClick}
+            aria-label={
+              colorMode === "dark" ? "Toggle Light Mode" : "Toggle Dark Mode"
+            }
+            icon={colorMode === "dark" ? <SunIcon /> : <MoonIcon />}
+            variant="ghost"
+          />
+        </Stack>
+      </Box>
     </Flex>
   );
 };
